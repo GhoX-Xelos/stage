6cfd4fae810dff629c843aea16be258076ff597d
Patate22 <alicia.courtois22@gmail.com>
2026-01-08 10:44:28 +0100

    style de header reparer

--- views/home/index.php
+++ views/home/index.php
@@ -13,6 +13,7 @@
 
 <main style="padding: 30px;">
     <h1>Bienvenue 👋</h1>
+    <p>Ceci est la page d'accueil de notre site web.</p>
 </main>
 
 <?php include __DIR__ . '/../layout/footer.php'; ?>
--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="public/css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
 <?php
 // Récupération des informations de contact depuis la base de données
 require_once __DIR__ . '/../../models/Database.php';
@@ -79,3 +92,6 @@ $reseaux = $stmtReseaux->fetchAll();
     </div>
   </div>
 </footer>
+
+</body>
+</html>
