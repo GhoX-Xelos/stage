73a643adc8f60196ce71af00146dd0268d0607fa
Patate22 <alicia.courtois22@gmail.com>
2026-01-08 09:59:45 +0100

    footer avec style

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
 <?php
 // Récupération des informations de contact depuis la base de données
 require_once __DIR__ . '/../../models/Database.php';
@@ -79,3 +91,6 @@ $reseaux = $stmtReseaux->fetchAll();
     </div>
   </div>
 </footer>
+
+</body>
+</html>
