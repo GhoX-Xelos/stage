f96feedcce8ba6fe2d6bc697c3246a0426f8649a
GhoX-Xelos <champagnac.wpro@gmail.com>
2026-01-08 16:54:05 +0100

    Organisation div carrousel

--- views/presentation/accueil.php
+++ views/presentation/accueil.php
@@ -14,18 +14,18 @@
 <section class="grid1">
     <div class="block" id="carrousel"> 
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="public/image/carousel1/photo1.jpg" class="d-block w-100" alt="photo1">
-    </div>
-    <div class="carousel-item">
-      <img src="public/image/carousel1/photo2.jpg" class="d-block w-100" alt="photo2">
-    </div>
-    <div class="carousel-item">
-      <img src="public/image/carousel1/photo3.jpg" class="d-block w-100" alt="photo3">
-    </div>
-  </div>
-</div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="public/image/carousel1/photo1.jpg" class="d-block w-100" alt="photo1">
+                </div>
+                <div class="carousel-item">
+                    <img src="public/image/carousel1/photo2.jpg" class="d-block w-100" alt="photo2">
+                </div>
+                <div class="carousel-item">
+                    <img src="public/image/carousel1/photo3.jpg" class="d-block w-100" alt="photo3">
+                </div>
+            </div>
+        </div>
         <!-- Contenu superposé -->
         <div class="carousel-overlay">
             <h1 class="carousel-title">Niak Niak Kadric</h1>
